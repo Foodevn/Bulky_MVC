a0d25f0db4068ddf578a668e3d9bf84139dbd4cd
Foodevn <hoangphuc0918065630@gmail.com>
2025-08-17 13:23:27 +0700

    init project in section 3
    Signed-off-by: Foodevn <hoangphuc0918065630@gmail.com>

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,5 +18,82 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name.ToLower() == obj.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "Tên danh mục không được trùng với thứ tự hiển thị");
+            if (obj.Name.ToLower().Equals("test"))
+                ModelState.AddModelError("", "giá trị test không hợp lệ");
+
+            if (!ModelState.IsValid)
+                return View(obj);
+           
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name.ToLower() == obj.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "Tên danh mục không được trùng với thứ tự hiển thị");
+            if (obj.Name.ToLower().Equals("test"))
+                ModelState.AddModelError("", "giá trị test không hợp lệ");
+
+            if (!ModelState.IsValid)
+                return View(obj);
+           
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category update successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+                return NotFound();
+           
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category delete successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
